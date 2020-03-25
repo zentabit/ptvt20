36600f150deacc281d68c2d6679b96dd9b50b6a8
Emil Babayev <zentabit@gmail.com>
2020-03-25 11:52:43 +0100

    Labb 8

--- pt-workspace/Lab08-memory/src/memory/MemoryBoard.java
+++ pt-workspace/Lab08-memory/src/memory/MemoryBoard.java
@@ -1,50 +1,115 @@
 package memory;
 
+import java.util.Random;
+
+import pair.Pair;
+import pair.PairSet;
 
 public class MemoryBoard {
-
+	private MemoryCardImage board[][];
+	private boolean ori[][];
+	private int sz;
+	
 	/** Skapar ett memorybräde med size * size kort. backFileName är filnamnet 
 	    för filen med baksidesbilden. Vektorn frontFileNames innehåller filnamnen 
 	    för frontbilderna. */
 	public MemoryBoard(int size, String backFileName, String[] frontFileNames) {
+		sz = size;
+		board = new MemoryCardImage[sz][sz];
+		ori = new boolean[sz][sz];
+		//createCards(backFileName, frontFileNames);
+		createCards2(backFileName, frontFileNames);
 
 	}
 
 	/* Skapar size * size / 2 st memorykortbilder.
 	   Placerar ut varje kort på två slumpmässiga ställen på spelplanen. */
 	private void createCards(String backFileName, String[] frontFileNames) {
-
+		Random rand = new Random();
+		
+		for(String s: frontFileNames) {
+			MemoryCardImage img = new MemoryCardImage(s, backFileName);
+			int c = rand.nextInt(sz);
+			int r = rand.nextInt(sz);
+			
+			while(board[r][c] != null) {
+				c = rand.nextInt(sz);
+				r = rand.nextInt(sz);
+			}
+			board[r][c] = img;
+			ori[r][c] = false;
+			
+			while(board[r][c] != null) {
+				c = rand.nextInt(sz);
+				r = rand.nextInt(sz);
+			}
+			board[r][c] = img;
+			ori[r][c] = false;
+			
+		}
 	}
 
+	private void createCards2(String backFileName, String[] frontFileNames) {
+		Random rand = new Random();
+		PairSet ps = new PairSet(sz, sz);
+		
+		for(String s: frontFileNames) {
+			MemoryCardImage img = new MemoryCardImage(s, backFileName);
+			Pair p1 = ps.pick();
+			Pair p2 = ps.pick();
+			
+			board[p1.first()][p1.second()] = img;
+			board[p2.first()][p2.second()] = img;
+		}
+	}
 	/** Tar reda på brädets storlek. */
 	public int getSize() {
-		return 0;
+		return sz;
 	}
 	
 	/** Hämtar den tvåsidiga bilden av kortet på rad r, kolonn c.
 	    Raderna och kolonnerna numreras från 0 och uppåt. */
 	public MemoryCardImage getCard(int r, int c) {
-		return null;
+		return board[r][c];
 	}
 
 	/** Vänder kortet på rad r, kolonn c. */
 	public void turnCard(int r, int c) {
-
+		ori[r][c] = !ori[r][c];
 	}
 	
 	/** Returnerar true om kortet r, c har framsidan upp. */
 	public boolean frontUp(int r, int c) {
-		return false;
+		return ori[r][c];
 	}
 	
 	/** Returnerar true om det är samma kort på rad r1, kolonn c2 som på rad r2, 
 	    kolonn c2. */
 	public boolean same(int r1, int c1, int r2, int c2) {
-		return false;
+		return board[r1][c1] == board[r2][c2];
 	}
 
 	/** Returnerar true om alla kort har framsidan upp. */
 	public boolean hasWon() {
-		return false;
+		for(boolean[] r: ori) {
+			for(boolean b: r) {
+				if(b == false) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}	
+	
+	public void print() {
+		for(int i = 0; i<board.length; i++) {
+			for(int j = 0; j<board[0].length; j++) {
+				if(board[i][j]!=null) {
+					System.out.print("hej ");
+				}
+				
+			}
+			System.out.println();
+		}
+	}
 }
--- pt-workspace/Lab08-memory/src/memory/MemoryGame.java
+++ pt-workspace/Lab08-memory/src/memory/MemoryGame.java
@@ -1,11 +1,51 @@
 package memory;
 
+import java.io.File;
+
+import javax.swing.JOptionPane;
+
 public class MemoryGame {
 	public static void main(String[] args) {
-		String[] frontFileNames = {"can.jpg", "flopsy_mopsy_cottontail.jpg",
-				"friends.jpg", "mother_ladybird.jpg", "mr_mcgregor.jpg",
-				"mrs_rabbit.jpg", "mrs_tittlemouse.jpg", "radishes.jpg" };
-		
-		// Fyll i egen kod här
+		String[] frontFileNames = { "can.jpg", "flopsy_mopsy_cottontail.jpg", "friends.jpg", "mother_ladybird.jpg",
+				"mr_mcgregor.jpg", "mrs_rabbit.jpg", "mrs_tittlemouse.jpg", "radishes.jpg" };
+
+		int n = 0;
+
+		while (n == 0) {
+			MemoryBoard b = new MemoryBoard(4, "back.jpg", frontFileNames);
+			MemoryWindow w = new MemoryWindow(b);
+			int rounds = 0;
+
+			w.drawBoard();
+
+			while (!b.hasWon()) {
+				w.waitForMouseClick();
+				int row1 = w.getMouseRow();
+				int col1 = w.getMouseCol();
+				w.waitForMouseClick();
+				int row2 = w.getMouseRow();
+				int col2 = w.getMouseCol();
+
+				if (!(b.frontUp(row1, col1) || b.frontUp(row2, col2))) {
+					b.turnCard(row1, col1);
+					b.turnCard(row2, col2);
+				} else {
+					continue;
+				}
+				w.drawBoard();
+
+				if (!b.same(row1, col1, row2, col2)) {
+					MemoryWindow.delay(1000);
+					b.turnCard(row1, col1);
+					b.turnCard(row2, col2);
+					w.drawBoard();
+				}
+				rounds++;
+			}
+
+			n = JOptionPane.showConfirmDialog(null, "That took " + rounds + " rounds. Wanna go again?", "You won!",
+					JOptionPane.YES_NO_OPTION);
+		}
+		System.exit(0);
 	}
 }
